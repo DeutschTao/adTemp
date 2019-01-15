279ccbaf101f42283c665625a48d3c216ba9042c
wangyangtao <wangyangtao@bytedance.com>
2019-01-15 13:48:54 +0800

    btn padding 40

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 var gulp = require('gulp'),
-    rename = require('gulp-rename');
-    minimist = require('minimist');
+    rename = require('gulp-rename'),
+    minimist = require('minimist'),
+    runSequence = require('run-sequence'),
+    rev = require('gulp-rev'),
+    clean = require('gulp-clean'),
+    revCollector = require('gulp-rev-collector');
+
 var options = require('minimist')(process.argv.slice(2));
+
+
+var xmlSrc = './public/dev/*.xml';
+
+gulp.task('clean', function(){
+    return gulp.src('./dist/' + options.newname, {read: false})
+        .pipe(clean());
+})
+gulp.task('revXml', function(){
+    return gulp.src(xmlSrc)
+        .pipe(rev())
+        .pipe(gulp.dest('./dist/'+ options.newname));
+})
     
 gulp.task('rename', function() {
     return gulp.src(["./public/dev/index.xml", "./public/dev/index.json"])
@@ -10,4 +28,18 @@ gulp.task('rename', function() {
           }))
         .pipe(gulp.dest("./dist/" + options.newname));
 });
+
+// gulp.task('dev', function (done) {  
+//     condition = false;  
+//     runSequence(  
+//         ['clean'],  
+//         ['revXml'],   
+//         done  
+//     );  
+// });
+
+// gulp.task('default', ['dev']);
+// gulp.task('runsequence', function (callback) {
+//     runSequence('clean', 'revXml', callback)
+// })
 
