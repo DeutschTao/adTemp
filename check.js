29fa3492e8be8e8eee2def7c10e25a8ee98dd350
wangyangtao <wangyangtao@bytedance.com>
2019-01-17 17:13:15 +0800

    xmlReader check

---
+++ check.js
@@ -0,0 +1,13 @@
+var xmlreader = require("xmlreader");
+var fs = require("fs");
+
+fs.readFile('./dev/template/' + process.env.pathName + '/index.xml','utf-8',function(err,result){
+    xmlreader.read(result, function(errors, response){
+        if(null !== errors ){
+            console.log(errors)
+            return;
+        }
+        console.log('ok');
+        // console.log(JSON.parse(response.VanGogh.Style.text()))
+    });
+ });
