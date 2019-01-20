222d0b2142d520b77910d44431eb0b72b95082c2
wangyangtao <wangyangtao@bytedance.com>
2019-01-20 10:09:23 +0800

    xml2js test

--- check.js
+++ check.js
@@ -1,5 +1,13 @@
 var xmlreader = require("xmlreader");
 var fs = require("fs");
+var xml2js = require('xml2js');
+
+  var builder = new xml2js.Builder();  // JSON->xml
+  var parser = new xml2js.Parser();   //xml -> json
+
+//   var json =  parser.parseString(xml);
+//   xml =  builder.buildObject(json);
+
 
 fs.readFile('./dev/template/' + process.env.pathName + '/index.xml','utf-8',function(err,result){
     xmlreader.read(result, function(errors, response){
@@ -8,9 +16,16 @@ fs.readFile('./dev/template/' + process.env.pathName + '/index.xml','utf-8',func
             return;
         }
         console.log('ok');
-        console.log(response.VanGogh)
+        console.log(response)
+        var xml =  builder.buildObject(response);
+        console.log(xml)
         // console.log(response.VanGogh.Style.text())
     });
+    // var json =  parser.parseString(result);
+    // console.log(json)
+    //  var xml =  builder.buildObject(json);
+    // console.log(xml)
+
  });
 
 
